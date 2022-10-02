2ac8133d3bf0c0a412dc92646f793e7731daee5f
VinCoD <5895.2020@students.ku.ac.ke>
2022-10-02 21:27:25 +0300

    Configured the new app's setings,  Urls, and views

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # my apps
     'blog.apps.BlogConfig',
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
--- django_project/urls.py
+++ django_project/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path('', include('blog.urls')),
+    path('', include('accounts.urls')),
 ]
